2c070ae5c0868df1bb620d0bbb9aad4cb28a1463
Casper <casper_too@hotmail.com>
2019-09-23 16:41:18 +0200

    Minor spelling correction

--- StopWatch.lua
+++ StopWatch.lua
@@ -17,7 +17,7 @@ function StopWatch:Initialize()
 			local scale = string.sub(msg, 7)
 			StopWatchFrame:SetScale(tonumber(scale))
 		else
-			StopWatch:SendMessage("Those commands are available:")
+			StopWatch:SendMessage("These commands are available:")
 			StopWatch:SendMessage("- show (BSP: /stw show)")
 			StopWatch:SendMessage("- hide (BSP: /stw hide)")
 			StopWatch:SendMessage("- scale (BSP: /stw scale 1.5)")
